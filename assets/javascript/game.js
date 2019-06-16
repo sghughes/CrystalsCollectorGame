313a843e8db37dda3d5b26bc3e65c77a0e066e96
Stephanie Hughes <stephaniegracehughes@gmail.com>
2019-06-16 14:15:35 -0700

    added if statement for when game is won

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ var crystal3;
 var crystal4;
 var wins = 0; //increase by 1 when totalScore = gameNumber
 var losses = 0; //increase by 1 when totalScore > gameNumber
-var gameNumber; //value per game should be randomly generated between 19-120
+var gameNumber = 0; //value per game should be randomly generated between 19-120
 var totalScore = 0; //increases by value of crystal when that crystal is clicked
 
 //functions:
@@ -21,9 +21,9 @@ function start(){
     crystal3 = Math.floor(Math.random()*12)+1;
     crystal4 = Math.floor(Math.random()*12)+1;
     console.log('crystal 1 number: ' + crystal1)
-    console.log('crystal 1 number: ' + crystal2)
-    console.log('crystal 1 number: ' + crystal3)
-    console.log('crystal 1 number: ' + crystal4)
+    console.log('crystal 2 number: ' + crystal2)
+    console.log('crystal 3 number: ' + crystal3)
+    console.log('crystal 4 number: ' + crystal4)
     $('.currentScore').html(totalScore);
     $('.winsHTML').html('Wins: '+ wins);
     $('.lossesHTML').html('Losses: ' + losses);
@@ -34,17 +34,29 @@ function play(){
     $('.crystalImage1').on('click',function()
     {
         totalScore+= crystal1;
-        console.log(totalScore);
+        console.log('total score: ' + totalScore);
+        console.log('game num: ' + gameNumber);
+
+
         if(totalScore<gameNumber){
             $('.currentScore').html(totalScore);
         }
-        if(totalScore>gameNumber){
+        else if(totalScore>gameNumber){
             console.log('you lost');
             losses++;
             $('.lossesHTML').html('Losses: ' + losses);
             totalScore = 0;
             start();
         }
+        else{
+            wins++;
+            $('.winsHTML').html('Wins: ' + wins);
+            totalScore = 0;
+            start();
+        }
+        
+
+       
     })
 
     $('.crystalImage2').on('click',function()
@@ -54,10 +66,16 @@ function play(){
         if(totalScore<gameNumber){
             $('.currentScore').html(totalScore);
         }
-        if(totalScore>gameNumber){
+        else if(totalScore>gameNumber){
             console.log('you lost');
             losses++;
             $('.lossesHTML').html('Losses: ' + losses);
+            totalScore = 0;
+            start();
+        }
+        else{
+            wins++;
+            $('.winsHTML').html('Wins: ' + wins);
             totalScore = 0;
             start();
         }
@@ -70,10 +88,16 @@ function play(){
         if(totalScore<gameNumber){
             $('.currentScore').html(totalScore);
         }
-        if(totalScore>gameNumber){
+        else if(totalScore>gameNumber){
             console.log('you lost');
             losses++;
             $('.lossesHTML').html('Losses: ' + losses);
+            totalScore = 0;
+            start();
+        }
+        else{
+            wins++;
+            $('.winsHTML').html('Wins: ' + wins);
             totalScore = 0;
             start();
         }
@@ -86,10 +110,16 @@ function play(){
         if(totalScore<gameNumber){
             $('.currentScore').html(totalScore);
         }
-        if(totalScore>gameNumber){
+        else if(totalScore>gameNumber){
             console.log('you lost');
             losses++;
             $('.lossesHTML').html('Losses: ' + losses);
+            totalScore = 0;
+            start();
+        }
+        else{
+            wins++;
+            $('.winsHTML').html('Wins: ' + wins);
             totalScore = 0;
             start();
         }
@@ -107,9 +137,9 @@ play();
 
 //play function: when crystal clicked, add crystal value to total score. Check if total score = or > gameNumber. Display total score.
 
-//Randomly generated number at start of game
+
 //Randomly generated number when you win or lose
 //Reset function for crystal numbers, total score, and number to guess
-//on click event for each crystal
+
 //store value when crystal is clicked and add to total score. 
 // alert if total score matches, you win displays, win count goes up by 1, everything else resets. If over, you lose.
